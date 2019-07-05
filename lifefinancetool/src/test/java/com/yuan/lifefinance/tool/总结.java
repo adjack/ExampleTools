6371a2912064fc8f48cc69cf9868549c904a8d31
admin <916000874@qq.com>
2019-07-05 17:07:46 +0800

    test

---
+++ lifefinancetool/src/test/java/com/yuan/lifefinance/tool/总结.java
@@ -0,0 +1,18 @@
+package com.yuan.lifefinance.tool;
+
+/**
+ * copyright:华润创业(深圳)有限公司
+ * author:admin
+ * create_date:2019/7/5 16:40
+ * <p>
+ * describe:TODO
+ */
+public class 总结 {
+//    短线关注点：
+//    1.周线成交量形态
+//    2.尽量龙头
+//    3.低位上影线和下影线等长的上量形态
+//
+//
+//
+}

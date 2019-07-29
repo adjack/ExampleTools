513c6466050b99f5332f6284481858ae19d87873
admin <916000874@qq.com>
2019-07-29 18:23:38 +0800

    test

---
+++ lifefinancetool/src/test/java/com/yuan/lifefinance/tool/FundStock.java
@@ -0,0 +1,27 @@
+package com.yuan.lifefinance.tool;
+
+import org.junit.Test;
+
+/**
+ * copyright:华润创业(深圳)有限公司
+ * author:admin
+ * create_date:2019/7/29 10:03
+ * <p>
+ * describe:TODO
+ */
+public class FundStock {
+    @Test
+    public void test012(){
+        double[] value = {0.03,-0.02};
+    }
+//个人基金建仓  总资金：141000
+//---------------------------------第一批20190729
+//科大讯飞39.40
+//盐津铺子42.90
+//同花顺133
+//中国联通7.30
+//东方证券12.75
+//中国国航11.80
+//中科曙光39.40
+//---------------------------------第二批
+}
